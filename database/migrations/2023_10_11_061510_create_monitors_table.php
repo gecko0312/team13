5346787caf39b41d6821235456c69b730c80046a
pogua <mm60306mm@gmail.com>
2023-10-18 10:37:03 +0800

    first commit

---
+++ database/migrations/2023_10_11_061510_create_monitors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMonitorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('monitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_model',100);
+            $table->string('panel',100);
+            $table->string('resolution',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('monitors');
+    }
+}
