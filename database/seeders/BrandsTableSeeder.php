f34f6622638c052c5d612ee09149498cb222f643
pogua <mm60306mm@gmail.com>
2023-11-08 11:46:32 +0800

    更新自動生成monitors和brands資料表程式(生成資料皆是隨機生成

--- database/seeders/BrandsTableSeeder.php
+++ database/seeders/BrandsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class BrandsTableSeeder extends Seeder
 {
@@ -12,11 +13,50 @@ class BrandsTableSeeder extends Seeder
      *
      * @return void
      */
+
+    public function RandombrandsName(){
+        $length=rand(3,10);
+        $ENG_char='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $ENG_charLength=strlen($ENG_char);
+        $brandsName='';
+        for ( $i=0 ; $i <$length ; $i++ ){
+            $brandsName.=$ENG_char[rand(0,$ENG_charLength-1)];
+        }
+        return $brandsName;
+    }
+
+    public function RandombrandsLocation(){
+        $country=array(
+            '台灣',
+            '美國',
+            '荷蘭',
+            '日本',
+            '韓國',
+            '德國',
+            '中國',
+            '新加坡',
+            '瑞典',
+            '英國',
+        );
+        return $country[rand(0,count($country)-1)];
+    }
+
+
+    
+
     public function run()
     {
-        DB::table('brands')->insert([
-            'bname' => 'ASUS華碩',
-            'location' => '台灣'
-        ]);
+        for ($i=0;$i<10;$i++){
+            $bname=$this->RandombrandsName();
+            $location=$this->RandombrandsLocation();
+            $year=Carbon::now()->year;
+
+            DB::table('brands')->insert([
+                'bname' => $bname,
+                'location' => $location,
+                'brand_time'=>$year-rand(40,100)
+            ]);
+        }
+        
     }
 }
--- database/seeders/MonitorsTableSeeder.php
+++ database/seeders/MonitorsTableSeeder.php
@@ -12,15 +12,106 @@ class MonitorsTableSeeder extends Seeder
      *
      * @return void
      */
+
+    public function RandomProductModel(){
+        $ENG_char='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $ENG_charLength=strlen($ENG_char);
+        $fir_char='';
+        $back_char='';
+        $ProductModel='';//最終合成名稱
+
+        //中間數字
+        $productModel_num=rand(10,99);
+
+        //前兩個英文字
+        for( $i=0; $i<2; $i++){
+            $fir_char.=$ENG_char[rand(0,$ENG_charLength-1)];
+        }
+
+        //後2~4個英文字
+        for( $i=0; $i<rand(2,4); $i++){
+            $back_char.=$ENG_char[rand(0,$ENG_charLength-1)];
+        }
+        $ProductModel=$fir_char.$productModel_num.$back_char;
+
+        return  $ProductModel;
+    }
+
+    public function RandomSize(){
+        $product_size=array(
+            27,
+            23.8,
+            21.5,
+            32,
+            24
+        );
+        return $product_size[rand(0,count($product_size)-1)];
+    }
+
+    public function RandomHz(){
+        $product_hz=array(
+            75,
+            60,
+            100,
+            170,
+            165,
+            240,
+            250
+        );
+        return $product_hz[rand(0,count($product_hz)-1)];
+    }
+
+    public function RandomResolution(){
+        $product_resolution=array(
+            '1920*1080',
+            '2560*1440',
+            '3840*2160',
+        );
+        return $product_resolution[rand(0,count($product_resolution)-1)];
+    }
+
+    public function RandomNits(){
+        $product_nits=array(
+            250,
+            200,
+            300,
+            350
+        );
+        return $product_nits[rand(0,count($product_nits)-1)];
+    }
+
+    public function RandomPanel(){
+        $product_panel=array(
+            'IPS',
+            'VA',
+            'MVA'
+        );
+        return $product_panel[rand(0,count($product_panel)-1)];
+    }
+
     public function run()
     {
-        DB::table('monitors')->insert([
-            'product_model' => 'VA27EHE',
-            'bid' => 1,
-            'size' => 27.0,
-            'hz' => 75,
-            'resolution' => '1920*10800'
+        for( $i=0 ; $i <10 ; $i++){
+            $product_model=$this->RandomProductModel();
+            $size=$this->RandomSize();
+            $hz=$this->RandomHz();
+            $resolution=$this->RandomResolution();
+            $nits=$this->RandomNits();
+            $panel=$this->RandomPanel();
 
-        ]);
+            DB::table('monitors')->insert([
+                'product_model' => $product_model,
+                'bid' => rand(1,10),
+                'size' => $size,
+                'hz' => $hz,
+                'resolution' => $resolution,
+                'nits' => $nits,
+                'panel' => $panel,
+                'speaker' => rand(0,1),
+                'price' => rand(2500,9000)
+    
+            ]);
+        }
+        
     }
 }
