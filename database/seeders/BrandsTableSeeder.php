93259e78d02fd65761cd59a49a063f1f442380e4
pogua <mm60306mm@gmail.com>
2023-11-08 09:57:03 +0800

    首次增加自動生成monitors和brands資料表資料程式

---
+++ database/seeders/BrandsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BrandsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('brands')->insert([
+            'bname' => 'ASUS華碩',
+            'location' => '台灣'
+        ]);
+    }
+}
