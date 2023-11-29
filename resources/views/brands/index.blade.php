fe5cca72a3a818ff0e22306428e7f59a0bfe7a1b
DESKTOP-RDKKM9A\aaaa <acxntgkfy6038@gmail.com>
2023-11-29 23:23:59 +0800

    新增app,foorer,header版模，並套用在在brands.index和monitors.index的view

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -1,16 +1,31 @@
-<html>
-    <head>
 
-    </head>
-    <body>
+@extends('app')
+
+@section('title','電腦螢幕查詢網站 - 顯示所有廠牌資訊')
+
+@section('monitor_contents')
         <h1>顯示所有廠牌資訊</h1>
-        @for($i=0; $i<count($brands); $i++)
-            <span>編號：</span>{{ $brands[$i]['id'] }} </br>
-            <span>名稱：</span>{{ $brands[$i]['bname'] }} </br>
-            <span>地區：</span>{{ $brands[$i]['location'] }} </br>
-            <span>成立時間：</span>{{ $brands[$i]['brand_time'] }} </br>
-            </br>
-        @endfor
-
-    </body>
-</html>
+        <table>
+            <tr>
+                <th>編號</th>
+                <th>名稱</th>
+                <th>地區</th>
+                <th>成立時間</th>
+                <th>操作1</th>
+                <th>操作2</th>
+                <th>操作3</th>
+            </tr>
+            @for($i=0; $i<count($brands); $i++)
+                <tr>
+                    <td>{{ $brands[$i]['id'] }}</td>
+                    <td>{{ $brands[$i]['bname'] }}</td>
+                    <td>{{ $brands[$i]['location'] }}</td>
+                    <td>{{ $brands[$i]['brand_time'] }}</td>
+                    <td><a href="{{ route('brands.show',['id'=>$brands[$i]['id']]) }}">顯示</a></td>
+                    <td><a href="{{ route('brands.edit',['id'=>$brands[$i]['id']]) }}">修改</a></td>
+                    <td>刪除</td>
+                </tr>
+            @endfor
+        </table>
+@endsection
+    
--- resources/views/monitors/index.blade.php
+++ resources/views/monitors/index.blade.php
@@ -1,22 +1,41 @@
-<html>
-    <head>
+@extends('app')
 
-    </head>
-    <body>
+@section('title','電腦螢幕查詢網站 - 顯示所有電腦螢幕資訊')
+
+@section('monitor_contents')
         <h1>顯示所有電腦螢幕資訊</h1>
-        @for($i=0; $i<count($monitors); $i++)
-            <span>編號：</span>{{ $monitors[$i]['id'] }} </br>
-            <span>產品型號：</span>{{ $monitors[$i]['product_model'] }} </br>
-            <span>廠牌：</span>{{ $monitors[$i]['bid'] }} </br>
-            <span>尺寸：</span>{{ $monitors[$i]['size'] }} </br>
-            <span>亮度：</span>{{ $monitors[$i]['nits'] }} </br>
-            <span>更新率：</span>{{ $monitors[$i]['hz'] }} </br>
-            <span>面板：</span>{{ $monitors[$i]['panel'] }} </br>
-            <span>喇叭：</span>{{ $monitors[$i]['speaker'] }} </br>
-            <span>解析度：</span>{{ $monitors[$i]['resolution'] }} </br>
-            <span>價錢：</span>{{ $monitors[$i]['price'] }} </br>
-            </br>
-        @endfor
-
-    </body>
-</html>
+        <table>
+            <tr>
+                <th>編號</th>
+                <th>產品型號</th>
+                <th>廠牌</th>
+                <th>尺寸</th>
+                <th>亮度</th>
+                <th>更新率</th>
+                <th>面板</th>
+                <th>喇叭</th>
+                <th>解析度</th>
+                <th>價錢</th>
+                <th>操作1</th>
+                <th>操作2</th>
+                <th>操作3</th>
+            </tr>
+            @for($i=0; $i<count($monitors); $i++)
+                <tr>
+                    <td>{{ $monitors[$i]['id'] }}</td>
+                    <td>{{ $monitors[$i]['product_model'] }}</td>
+                    <td>{{ $monitors[$i]['bid'] }}</td>
+                    <td>{{ $monitors[$i]['size'] }}</td>
+                    <td>{{ $monitors[$i]['nits'] }}</td>
+                    <td>{{ $monitors[$i]['hz'] }}</td>
+                    <td>{{ $monitors[$i]['panel'] }}</td>
+                    <td>{{ $monitors[$i]['speaker'] }}</td>
+                    <td>{{ $monitors[$i]['resolution'] }}</td>
+                    <td>{{ $monitors[$i]['price'] }}</td>
+                    <td><a href="{{ route('monitors.show',['id'=>$monitors[$i]['id']]) }}">顯示</a></td>
+                    <td><a href="{{ route('monitors.edit',['id'=>$monitors[$i]['id']]) }}">編輯</a></td>
+                    <td>刪除</td>
+                </tr>
+            @endfor
+        </table>
+@endsection
